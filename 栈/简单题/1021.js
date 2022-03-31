c5b88a5c45e4e539fe3de04801ac89a4fcb70884
XinXiaoIsMe <ccattempt@163.com>
2022-03-31 20:33:13 +0800

    add Q1021

---
+++ 栈/简单题/1021.js
@@ -0,0 +1,35 @@
+// 这题和 1614 题类似
+var removeOuterParentheses = function (s) {
+  let ret = ''
+  let size = 0
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      if (size > 0) ret += '('
+      size++
+    } else if (s[i] === ')') {
+      size--
+      if (size > 0) ret += ')'
+    }
+  }
+
+  return ret
+}
+
+// 同样，这题也不需要使用一个栈
+var removeOuterParentheses = function (s) {
+  let ret = ''
+  const stack = []
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      if (stack.length > 0) ret += '('
+      stack.push('(')
+    } else if (s[i] === ')') {
+      stack.pop()
+      if (stack.length > 0) ret += ')'
+    }
+  }
+
+  return ret
+}
